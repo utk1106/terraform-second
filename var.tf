bc2242a0ee0c7d333ee4f668309e8c37801d44ab
utkarsh <thaokarutkarsh@gmail.com>
2024-02-20 12:00:53 -0800

    new

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "resource-group-name" {
+  default = "terraform-resource-group"
+  description = "The prefix used for all resources is this example"
+}
+
+variable "location" {
+  default = "Central India"
+  description = "The azure location where all resources in this example should be created"
+}
